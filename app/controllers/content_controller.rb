b97ca5989bd74600079f6247b48b84d746cd214e
Luzie Deubel <luzie@deubels.de>
2021-01-18 14:07:48 +0100

    added controller actions

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -1,2 +1,28 @@
 class ContentController < ApplicationController
+  def show
+    @content = Content.find(params[:id])
+  end
+
+  def random_content
+    @content = Content.sample
+  end
+
+  def new
+    @content = Content.new
+  end
+
+  def create
+    @content = Content.new(content_params)
+    if @content.save
+      redirect_to content_path(@content)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def content_params
+    params.require(:content).permit(:description)
+  end
 end
